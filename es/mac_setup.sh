a84d71530d1c07623e7b58bdffd1c4b79f4c32a2
y2ghost <snrtcel@126.com>
2023-08-10 21:53:25 +0800

    添加分词器、更新查询、多字段等示例

--- es/mac_setup.sh
+++ es/mac_setup.sh
@@ -1,12 +1,24 @@
 es_tgz="elasticsearch-7.17.12-darwin-x86_64.tar.gz"
 kibana_tgz="kibana-7.17.12-darwin-x86_64.tar.gz"
+logstash_tgz="logstash-7.17.12-darwin-x86_64.tar.gz"
+temp_dir="tmp_elk_$$"
+
+mkdir ${temp_dir}
+cd ${temp_dir}
 
 curl -fsSL -o ${es_tgz} https://artifacts.elastic.co/downloads/elasticsearch/${es_tgz}
 tar xfvz ${es_tgz}
-mv elasticsearch-7.17.12 elasticsearch
+rm -f ${es_tgz}
+mv elasticsearch-* elasticsearch
 
 curl -fsSL -o ${kibana_tgz} https://artifacts.elastic.co/downloads/kibana/${kibana_tgz}
 tar xfvz ${kibana_tgz}
-mv kibana-7.17.12-darwin-x86_64 kibana
-exit 0
+rm -f ${kibana_tgz}
+mv kibana-* kibana
 
+curl -fsSL -o ${logstash_tgz} https://artifacts.elastic.co/downloads/logstash/${logstash_tgz}
+tar xfvz ${logstash_tgz}
+rm -f ${logstash_tgz}
+mv logstash-* logstash
+
+exit 0
--- es/readme.txt
+++ es/readme.txt
@@ -1,8 +1,4 @@
 ES学习概述
-- mac_setup.sh快速安装脚本
-- crud.txt示例基本的操作
-- search.txt示例基本的搜索
-  > search.sh使用curl命令构建索引环境
-- text_number_queries.txt文本和数字查询示例
-- geo_queries.txt演示GEO地理数据查询示例
+- sh类型的文件用于安装环境或是生成测试数据
+- txt类型的文件使用kibana-console执行API操作示例
 
