57c1643b053cb70540038f6181ebecb863f43476
y2ghost <snrtcel@126.com>
2023-09-21 00:32:44 +0800

    添加动态库示例

---
+++ c/shlib/mod1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int test1[250000] = { 1, 2, 3 };
+
+#ifndef VERSION
+#define VERSION ""
+#endif
+
+void x1(void) {
+    printf("Called mod1-x1 " VERSION "\n");
+}
